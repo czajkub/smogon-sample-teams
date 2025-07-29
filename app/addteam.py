f4c07c8f8d994d2088586e4e69065134b2ad33bc
czajkub <czajkub@gmail.com>
2025-07-29 21:14:04 +0200

    adding entries to database now works

--- app/addteam.py
+++ app/addteam.py
@@ -1,14 +1,56 @@
+import sys
+import psycopg2
+
 from loadenv import load_env
+from pokeparser import parser
 
-config = load_env()
-connection = config.connect()
 
-cursor = connection.cursor()
-cursor.execute("CREATE TABLE products("
-               "id SERIAL PRIMARY KEY,"
-               "teamname TEXT NOT NULL,"
-               "author TEXT NOT NULL,"
-               "mons TEXT[]"
-)
-cursor.close()
-connection.commit()
+create_query = """
+CREATE TABLE teams (
+    id SERIAL PRIMARY KEY,
+    author TEXT NOT NULL,
+    title TEXT NOT NULL,
+    mons TEXT[]
+    );
+"""
+
+insert_query = """
+INSERT INTO teams (author, title, mons)
+VALUES (%s, %s, %s);
+"""
+
+check_query = """
+SELECT * FROM teams WHERE title=%s;
+"""
+
+
+def isduplicate(connection, title: str) -> bool:
+    cursor = connection.cursor()
+    query = cursor.execute(check_query, (title,))
+    cursor.close()
+    return query is not None
+
+
+def insert(connection, json: dict[str, str]) -> None:
+    if isduplicate(connection, json["title"]):
+        print(f"Team {json['title']} from url {url} already exists")
+        return
+
+    cursor = connection.cursor()
+    cursor.execute(insert_query, (json["author"], json["title"], json["mons"]))
+
+    cursor.close()
+    connection.commit()
+
+
+if __name__ == "__main__":
+    if sys.argv.__len__() == 1:
+        print("Provide urls as arguments to add team")
+        sys.exit(1)
+    urls = sys.argv[1:]
+    config = load_env()
+    connection = psycopg2.connect(config.DB_URL)
+    for url in urls:
+        json = parser.pasteJSON(url)
+        insert(connection, json)
+    connection.close()
--- app/pokeparser/parser.py
+++ app/pokeparser/parser.py
@@ -1,5 +1,4 @@
 import sys
-import json
 import re
 
 from bs4 import BeautifulSoup
@@ -29,7 +28,7 @@ types = [
 ]
 
 
-def pasteJson(url: str):
+def pasteJSON(url: str) -> dict[str, str]:
     response = requests.get(url)
 
     if response.status_code != 200:
@@ -39,23 +38,19 @@ def pasteJson(url: str):
     soup = BeautifulSoup(response.content, "html.parser")
 
     title = soup.title.string
-    print(f"title = {title}")
-
-    cos = soup.find_all("span", attrs={"class": "type-electric"})
-    cos2 = soup.find_all("span", attrs={"class": re.compile("type-*")})
-    # print(cos2)
     author = soup.h2.text[4:]
-    print(author)
 
     mons = []
-    for span in cos2:
+    spans = soup.find_all("span", attrs={"class": re.compile("type-*")})
+
+    for span in spans:
         if span.text == "-":
             continue
         if span.text in types:
             continue
         mons.append(span.text)
 
-    print({"mons": mons, "author": author, "title": title})
+    return {"mons": mons, "author": author, "title": title}
 
 
 if __name__ == "__main__":
@@ -63,4 +58,4 @@ if __name__ == "__main__":
         print("Provide only 1 argument: pokepaste url")
         sys.exit(1)
     url = sys.argv[1]
-    pasteJson(url)
+    pasteJSON(url)
